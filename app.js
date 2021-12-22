addd9854df1cfae2fe316e5a5a12c77920edc0e9
dv-kong <vincent.kong@outlook.com>
2021-12-22 10:47:03 +0100

    package update, env.example fix

--- app.js
+++ app.js
@@ -2,25 +2,31 @@ import express from "express";
 import env from "./src/config/env.js";
 import Server from "./src/config/server.js";
 import middlewares from "./src/config/middlewares.js";
+// console.log(middlewares);
+import cors from "cors";
 import routes from "./src/modules";
-import db from "./src/config/db.js";
-import handleError from "./src/middlewares/errorHandler.js";
 
+import db from "./src/config/db.js";
+// import routes from './src/modules/index'
+
+// import routes from './src/modules/User/router'
 const http = express();
+// http.use(cors());
 const server = new Server(http);
 // http.use(cors());
 
 server.middlewares(middlewares); // makes express use all middlewares contained in the middlewares object
 server.routes(routes);
-server.errorHandler(handleError);
+// server.middlewares(middlewares.apiLogger); // makes express use all middlewares contained in the middlewares object
+// server.start(env.db_port)
 
 (async () => {
   try {
     await db.associateAll(db.sequelize.models);
     // await db.sequelize.sync({alter:true});
     await db.sequelize.sync({
-      alter:true // TODO: 
-      // force: true
+
+      force: true
 
 
     });
@@ -30,7 +36,6 @@ server.errorHandler(handleError);
     await server.start(env.port);
     console.log(`Database started on port ${env.db_port}.`);
   } catch (error) {
-    
     console.error(error);
   }
 })();
